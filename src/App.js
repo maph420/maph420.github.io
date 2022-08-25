431357e17d56e7bfa0cabd1788202b00adaedb84
maph <juani.bertoni73@gmail.com>
2022-08-25 19:26:38 -0300

    created left bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Sidebar from "./Sidebar";
+import AppAbout from "./About"
+import ContentContainer from "./Content"
+import { Layout } from 'antd';
+
+const Content = Layout;
+
 
 export default function App() {
   return (
     <div className="flex">
       <Sidebar />
+      <Content>
+        <AppAbout />
+      </Content>
     </div>
+  
+
+    
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#334155",
+        secondary: '#ebedf0',
+      },
+    },
   },
   plugins: [],
 }
