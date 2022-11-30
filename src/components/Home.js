0ef1a002336198ca9b7ffe1fc6c5322a7d4c2660
maph <juani.bertoni73@gmail.com>
2022-11-29 23:27:29 -0300

    trying to add stack

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import pfp from "../im/profile_picture.png"
+import bashLogo from "../im/stack/bash.png"
+import CLogo from "../im/stack/C.png"
+import HTMLLogo from "../im/stack/HTML.png"
+import javaScriptLogo from "../im/stack/javascript.png"
+import mongoLogo from "../im/stack/mongo.png"
+import pythonLogo from "../im/stack/python.png"
+import streamlitLogo from "../im/stack/streamlit.png"
+
+
+const myStack = [
+  bashLogo,
+  CLogo,
+  HTMLLogo,
+  javaScriptLogo,
+  mongoLogo,
+  pythonLogo,
+  streamlitLogo
+];
+
+
+const StackRow = ({stack}) => (
+  <div>
+    {stack.map(stackElem => (
+      <img class="stack-logo" src={`${stackElem}`} alt="stack element"></img>
+    ))}
+  </div>
+);
+
 
 function AppHome() {
   return (
     <div id="home" className="sector">
       <div className="info-box">
-        
+
         <div class="user-img-container"> 
-          <img class='user-img' src='https://preview.redd.it/jzqjaawvivv61.png?width=631&format=png&auto=webp&s=be4f35d0379f5fcd4e15792e77634de30e1b5ed4' alt='profile pic'/>  
+          <img class='user-img' src={pfp} alt='profile pic'/>  
         </div>
 
         <div class="user-info">
@@ -16,7 +45,10 @@ function AppHome() {
           <p>jhasdkjashkjdashjkdajskh</p>
           <p>jhasdkjashkjdashjkdajskh</p>
           <p>jhasdkjashkjdashjkdajskh</p>
-          <p>jhasdkjashkjdashjkdajskh</p>
+          <div class="stack-div">
+            <StackRow stack={myStack}></StackRow>
+          </div>
+          
         </div>
 
       </div> 
