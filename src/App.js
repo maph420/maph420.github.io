8d0d1006835656d75e60fea278ffcbf83f4a983b
maph <juani.bertoni73@gmail.com>
2022-11-30 22:29:18 -0300

    added loading screen

--- src/App.js
+++ src/App.js
@@ -3,11 +3,25 @@ import AppAbout from "./components/About"
 import { Layout } from 'antd';
 import AppHome from "./components/Home";
 import AppWorks from "./components/Works";
+import {useEffect, useState} from 'react';
+import { MinimalSpinner } from 'loading-animations-react';
 
 const Content = Layout;
+
 export default function App() {
+  
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000)
+  }, []);
+  
   return (
+    <>
+    {loading === false ? (  
+    
     <div className="flex">
+      
       <Content>
         <Sidebar />
         <AppHome />
@@ -15,5 +29,10 @@ export default function App() {
         <AppWorks />
       </Content>
     </div>
-  )
+    ) : (
+      <MinimalSpinner className="loading-spinner" color="white" />
+    )}
+    </>
+  );
+  
 }
