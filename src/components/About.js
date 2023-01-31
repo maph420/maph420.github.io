f46cb5af0a8c9ba769dbd3a008558c587404cd4d
maph <juani.bertoni73@gmail.com>
2023-01-31 02:50:43 -0300

    added whoami section

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,7 @@
 import React from "react";
 import FadeIn from 'react-fade-in';
+import ReactCountryFlag from "react-country-flag"
+
 
 function AppAbout() {
   return (
@@ -7,19 +9,26 @@ function AppAbout() {
       <FadeIn>
       <div className="info-box">
         <div className="centered title">
-          <h2>About me</h2>
+          <h2 class="about-me-title">whoami?</h2>
 
           <div className="box">
-            <div className="box-text">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum
-              </p>
+            <div className="box-text about-me-box">
+              <p class="no-align"> 
+              🌌 Hey! My name's Juan Ignacio (call me Juani) I'm a <a class="link" href="https://web.fceia.unr.edu.ar/es/licenciatura-en-ciencias-de-la-computación.html" target={"_blank"}>Computer Science</a> student currently living in Argentina <ReactCountryFlag countryCode="AR" svg style=
+                {{width: '1.3em',height: '1.3em',}} title="AR"/> . I'm interested in fullstack web development 🛠🌐 (mostly backend) and I'm getting into the cybersecurity world! 🔒👨‍💻
+              <br/>
+              <br/>
+              <ul>
+                <li>I have fluent B2 english. <ReactCountryFlag countryCode="GB" svg style=
+                {{width: '1.3em',height: '1.3em',}} title="GB"/>
+                </li>
+                <li>I'm also learning chinese! <ReactCountryFlag countryCode="CN" svg style=
+                {{width: '1.3em',height: '1.3em',}} title="CN"/> 你好, 陌生人！
+                </li>
+              </ul>
+
+
+            </p>
             </div>
           </div>
         </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -20,13 +20,23 @@ const Sidebar = () => {
   );
 };
 
-const SideBarIcon = ({ icon, text = "tooltip", anchor = "#" }) => (
+const SideBarIcon = ({ icon, text = "tooltip", anchor = "#", bottomIcon = false }) => (
+  <>
+  { bottomIcon === false ? (
   <a href={anchor}>
     <div className="sidebar-icon group">
       {icon}
       <span class="sidebar-tooltip group-hover:scale-100">{text}</span>
     </div>
+  </a>) : (
+    <a href={anchor} className="bottom-icons">
+    <div className="sidebar-icon group">
+      {icon}
+      <span class="sidebar-tooltip group-hover:scale-100">{text}</span>
+    </div>
   </a>
+  )}
+  </>
 );
 
 export default Sidebar;
